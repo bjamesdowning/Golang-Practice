facb3dc969954ecda1ec1fa64f6062149f994bcf
bjamesdowning <bjamesdowning@gmail.com>
2018-04-24 00:02:29 +0300

    json encode/marshal/comment

--- Practice/07_struct/main.go
+++ Practice/07_struct/main.go
@@ -1,42 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 //learning structs
 
-type ip struct {
-	oct1 uint8
-	oct2 uint8
-	oct3 uint8
-	oct4 uint8
+type IP struct {
+	Oct1 uint8
+	Oct2 uint8
+	Oct3 uint8
+	Oct4 uint8
 }
 
-type rtr struct {
-	hname string
-	uname string
-	pword string
-	ip
+type RTR struct {
+	Hname string `json:"HOSTNAME"`
+	Uname string `json:"USERNAME"`
+	Pword string `json:"PASSWORD"`
+	IP
 }
 
 //not good.
-func (ip ip) dotNotation() (uint8, string, uint8, string, uint8, string, uint8) {
-	return ip.oct1, ".", ip.oct2, ".", ip.oct3, ".", ip.oct4
+func (IP IP) dotNotation() (uint8, string, uint8, string, uint8, string, uint8) {
+	return IP.Oct1, ".", IP.Oct2, ".", IP.Oct3, ".", IP.Oct4
 }
 
 func main() {
-	obj1 := rtr{
-		hname: "R1",
-		uname: "cisco",
-		pword: "cisco",
-		ip: ip{
-			oct1: 192,
-			oct2: 168,
-			oct3: 1,
-			oct4: 5,
+	obj1 := RTR{
+		Hname: "R1",
+		Uname: "cisco",
+		Pword: "cisco",
+		IP: IP{
+			Oct1: 192,
+			Oct2: 168,
+			Oct3: 1,
+			Oct4: 5,
 		},
 	}
 	//obj2 := foo{"Sara", "Dowell", "", 37}
-	fmt.Println(obj1.ip.dotNotation())
+	fmt.Println(obj1.IP.dotNotation())
 	fmt.Println(obj1)
+	bs, _ := json.Marshal(obj1)
+	fmt.Println(bs)
+	fmt.Printf("%T, \n", bs)
+	fmt.Println(string(bs))
+	fmt.Println("---------")
+	json.NewEncoder(os.Stdout).Encode(&obj1)
 	//fmt.Println(obj2.fname, obj2.age)
 }
