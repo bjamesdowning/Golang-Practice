15b84b81eca14f55c0d6625e8982224f1d23f063
bjamesdowning <bjamesdowning@gmail.com>
2018-04-23 09:54:55 +0300

    Added fun

--- Euler/Problem 2/main.go
+++ Euler/Problem 2/main.go
@@ -10,18 +10,20 @@ import (
 * find the sum of the even-valued terms.
  */
 func main() {
+	fmt.Println(evenFib(4000000))
+}
+func evenFib(limit int) int {
 	xs := []int{1, 2}
 	ans := 2
 	for {
 		fib := xs[1] + xs[0]
 		xs = append(xs, fib)
 		xs = append(xs[:0], xs[1:]...) //Avoid expanding slice beyond underlying arr
-		if fib > 4000000 {
+		if fib > limit {
 			break
 		} else if fib%2 == 0 {
 			ans += fib
 		}
 	}
-	fmt.Println(ans)
-
+	return ans
 }
