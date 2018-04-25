62f79eff514b3c9f93402fdad61815879a35fa16
bjamesdowning <bjamesdowning@gmail.com>
2018-04-25 08:01:12 +0300

    Budget edit

--- Practice/BudgetApp/main.go
+++ Practice/BudgetApp/main.go
@@ -8,23 +8,28 @@ import "fmt"
 // Practice using interfaces.
 
 //type bills to collect bills
-type bills struct {
-	bill float64
-}
+type bills []float64
 
 //Method for Bills to add them together and return total
 func (b bills) sumTotal() float64 {
-	return b.bill //test case
+	var sum float64
+	fmt.Println(len(b))
+	for i := 0; i < len(b); i++ {
+		sum += b[i]
+	}
+	return sum
 }
 
 //type income to collect income sources
-type income struct {
-	income float64
-}
+type income []float64
 
 //Method to add income sources
 func (i income) sumTotal() float64 {
-	return i.income //testcase
+	var sum float64
+	for j := 0; j < len(i); j++ {
+		sum += i[j]
+	}
+	return sum
 }
 
 //Interface to implement addition for each type
@@ -38,8 +43,8 @@ func added(t total) float64 {
 }
 
 func main() {
-	bill1 := bills{100.00}
-	income1 := income{1100.00}
+	bill1 := bills{100.00, 200.00, 100.00}
+	income1 := income{1100.00, 900.00}
 	totalIncome := added(income1)
 	totalBills := added(bill1)
 	savingsRec := (totalIncome - totalBills) * 0.2
