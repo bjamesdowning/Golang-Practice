4cee25df1386d62e69b1a1f47e17726d849e563d
bjamesdowning <bjamesdowning@gmail.com>
2018-04-30 04:47:31 +0300

    comments

--- Euler/Problem 4/main.go
+++ Euler/Problem 4/main.go
@@ -9,7 +9,7 @@ import (
 * Find the largest palindrome made from the product of two 3-digit numbers.
  */
 
-//Not a very good solution. Good enough for now.
+//Not a very good solution.
 func main() {
 	var sum int
 	var lrg int
@@ -19,12 +19,15 @@ func main() {
 			str := fmt.Sprintf("%v", sum)
 			rev := []byte{}
 			forw := []byte{}
+			//creates a reverse of the string from the origin sum
 			for k := len(str) - 1; k > 0; k-- {
 				rev = append(rev, str[k])
 			}
+			//turns str into a slive to compare to rev
 			for l := 0; l < len(str)-1; l++ {
 				forw = append(forw, str[l])
 			}
+			//use bytes package to compare slices
 			if bytes.Compare(rev, forw) == 0 {
 				if sum > lrg {
 					lrg = sum
