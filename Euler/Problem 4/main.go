fbdabc85bdee6e0329f7cda042a9b3f95666cdb7
bjamesdowning <bjamesdowning@gmail.com>
2018-04-30 04:33:20 +0300

    learning to incorporate other packages.

--- Euler/Problem 4/main.go
+++ Euler/Problem 4/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
@@ -11,6 +12,7 @@ import (
 //Not a very good solution. Good enough for now.
 func main() {
 	var sum int
+	var lrg int
 	for i := 999; i > 100; i-- {
 		for j := 999; j > 100; j-- {
 			sum = i * j
@@ -23,11 +25,12 @@ func main() {
 			for l := 0; l < len(str)-1; l++ {
 				forw = append(forw, str[l])
 			}
-			if rev[0] == forw[0] && rev[1] == forw[1] && rev[2] == forw[2] {
-				fmt.Print("Largest palendrom is ", sum)
-				fmt.Println("----", i, j)
-				break
+			if bytes.Compare(rev, forw) == 0 {
+				if sum > lrg {
+					lrg = sum
+				}
 			}
 		}
 	}
+	fmt.Print("Largest palendrom is ", lrg, "\n")
 }
