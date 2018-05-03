388c7baed82c2b77580e4c0568486a61abf33472
bjamesdowning <bjamesdowning@gmail.com>
2018-05-03 09:40:02 +0300

    Euler 3 first attempt at Pipeline

---
+++ Euler/Problem 3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+//takes way too long (~64min). Attempted pipline model.
+func main() {
+	f := factorsOf(600851475143)
+	pr := isPrime(f)
+	fmt.Println(<-pr)
+}
+
+func factorsOf(n int64) chan int64 {
+	out := make(chan int64)
+	go func() {
+		for i := int64(3); i < n; i += 2 {
+			if n%i == 0 {
+				out <- i
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func isPrime(ch chan int64) chan int64 {
+	out := make(chan int64)
+	var lrg int64
+	go func() {
+		for i := range ch {
+			for j := int64(3); j < i; j += 2 {
+				if i%j == 0 {
+					i = 0
+				}
+			}
+			if i > lrg {
+				lrg = i
+			}
+		}
+		out <- lrg
+		close(out)
+	}()
+	return out
+}
