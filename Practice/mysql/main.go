e777119c242f715bb5f1320b3b21f8ea49022d29
bjamesdowning <bjamesdowning@gmail.com>
2018-05-07 06:51:57 +0300

    connect to mysql docker container

---
+++ Practice/mysql/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//Program following book https://goo.gl/wepCxp
+//If using docker container running mysql, be sure to use 5.7
+//Latest container image pulls mysql 8 which throws an error about
+//Not supporting the authentication plugin. I'm not sure why.
+func main() {
+	db, err := sql.Open("mysql", "root:password@tcp(:3306)/test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(
+		"CREATE TABLE IF NOT EXISTS test.hello(world varchar(50))")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := db.Exec(
+		"INSERT INTO test.hello(world) VALUES('hello world!')")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("inserted %d rows", rowCount)
+
+	rows, err := db.Query("SELECT * FROM test.hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var s string
+		err = rows.Scan(&s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("found row containing %q", s)
+	}
+	rows.Close()
+}
