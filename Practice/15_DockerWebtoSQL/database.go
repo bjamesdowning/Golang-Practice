520e0df61327ab74b9669db447d97836a5b1d55b
bjamesdowning <bjamesdowning@gmail.com>
2018-05-18 09:23:24 +0300

    hostname

--- Practice/15_DockerWebtoSQL/database.go
+++ Practice/15_DockerWebtoSQL/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:password@tcp(172.17.0.2:3306)/test")
+	db, err := sql.Open("mysql", "root:password@tcp(go-mysql:3306)/test")
 	if err != nil {
 		log.Println(err)
 	}
