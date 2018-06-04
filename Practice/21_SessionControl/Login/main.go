bf7730c4c022b495e03f0e732dc76ac8b4a9480e
bjamesdowning <bjamesdowning@gmail.com>
2018-06-04 07:47:08 +0300

    normal signup.

--- Practice/21_SessionControl/Login/main.go
+++ Practice/21_SessionControl/Login/main.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"net/http"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/satori/go.uuid"
 )
 
@@ -11,6 +13,7 @@ type user struct {
 	Email string
 	Fname string
 	Lname string
+	Pword []byte
 }
 
 var tmpl *template.Template
@@ -24,37 +27,58 @@ func init() {
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/verify", verify)
+	http.HandleFunc("/signup", signUp)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	//Check for cookie
-	c, err := r.Cookie("session")
-	if err != nil {
-		sID, _ := uuid.NewV4() //throw away error.
-		c = &http.Cookie{
-			Name:  "session",
-			Value: sID.String(),
-		}
-		http.SetCookie(w, c)
+	u := getUser(w, r)
+	tmpl.ExecuteTemplate(w, "index.html", u)
+}
+
+func signUp(w http.ResponseWriter, r *http.Request) {
+	if loggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
-	//At this point, the user has a cookie
+
 	var u user
-	if email, ok := dbSessions[c.Value]; ok {
-		u = dbUsers[email]
-	}
 	//process form to create user
 	if r.Method == http.MethodPost {
 		email := r.FormValue("email")
 		fname := r.FormValue("firstname")
 		lname := r.FormValue("lastname")
-		u = user{email, fname, lname}
+		pword := r.FormValue("password")
+		//Check if username is already in the db
+		if _, ok := dbUsers[email]; ok {
+			http.Error(w, "Username Taken", http.StatusForbidden)
+			return
+		}
+		//Create a session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = email
+		//Store user info. First, encrpt pword
+		bs, err := bcrypt.GenerateFromPassword([]byte(pword), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Server Error, Password Related", http.StatusInternalServerError)
+			return
+		}
+		u = user{email, fname, lname, bs}
 		dbSessions[c.Value] = email
 		dbUsers[email] = u
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
 
-	tmpl.ExecuteTemplate(w, "index.html", u)
+	tmpl.ExecuteTemplate(w, "signup.html", u)
+
 }
 
 func verify(w http.ResponseWriter, r *http.Request) {
@@ -71,4 +95,33 @@ func verify(w http.ResponseWriter, r *http.Request) {
 	}
 	u := dbUsers[email]
 	tmpl.ExecuteTemplate(w, "verify.html", u)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) user {
+	//Check for cookie
+	c, err := r.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4() //throw away error.
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	//At this point, the user has a cookie
+	var u user
+	if email, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[email]
+	}
+	return u
+}
+
+func loggedIn(r *http.Request) bool {
+	c, err := r.Cookie("session")
+	if err != nil {
+		return false
+	}
+	email := dbSessions[c.Value]
+	_, ok := dbUsers[email]
+	return ok
 }
