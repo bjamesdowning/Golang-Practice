beda9d36ece4b8ccc041c9d34412957d3c123c30
bjamesdowning <bjamesdowning@gmail.com>
2018-06-13 20:45:33 +0300

    mongo added

--- Practice/24_go-mongo/main.go
+++ Practice/24_go-mongo/main.go
@@ -4,15 +4,34 @@ import (
 	"fmt"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
+type User struct {
+	Name   string        `json:"name" bson:"name"`
+	Gender string        `json:"gender" bson":gender"`
+	Age    int           `json:"age" bson:"age"`
+	Id     bson.ObjectId `json:"id" bson:"id"`
+}
+
 func main() {
-	session, err := mgo.Dial("localhost:27017")
+	session, err := mgo.Dial("localhost:32768")
 	if err != nil {
 		fmt.Println("session error: ", err)
 	}
-	c := session.DB("testDB1").C("testCOL1")
-	fmt.Println(c)
-	fmt.Println(c.Find(nil).Count())
+	u := User{
+		Name:   "John",
+		Gender: "Female",
+		Age:    22,
+		Id:     bson.NewObjectId(),
+	}
+
+	v := User{}
+	session.DB("go-mongo").C("users").Insert(u)
+	fmt.Println("*************Getting User******************")
+
+	if err = session.DB("go-mongo").C("users").FindId("5b21577a0e48dc396663b77d").One(&v); err != nil {
+		fmt.Println(err)
+	}
 
 }
