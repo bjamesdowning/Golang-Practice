07fab470f65ab1d20414fa9bdebe0305d9dff762
bjamesdowning <bjamesdowning@gmail.com>
2018-06-21 21:36:02 +0300

    mgo, create, read, check for already exist

--- Practice/24_go-mongo/main.go
+++ Practice/24_go-mongo/main.go
@@ -12,24 +12,54 @@ type User struct {
 	Name   string        `json:"name" bson:"name"`
 	Gender string        `json:"gender" bson:"gender"`
 	Age    int           `json:"age" bson:"age"`
-	ID     bson.ObjectId `json:"id" bson:"id"`
+	ID     bson.ObjectId `json:"_id" bson:"_id"`
 }
 
+var ErrNotFound error
+
 func main() {
-	session, err := mgo.Dial("localhost:32768")
+	session, err := mgo.Dial("localhost:27017")
 	if err != nil {
 		fmt.Println("session error: ", err)
 	}
+	defer session.Close()
+
 	u := User{
-		Name:   "John",
-		Gender: "Female",
-		Age:    22,
+		Name:   "Dexter1",
+		Gender: "Male",
+		Age:    55,
 		ID:     bson.NewObjectId(),
 	}
 
-	v := User{}
-	session.DB("go-mongo").C("users").Insert(u)
-	fmt.Println("*************Getting User******************")
+	c := session.DB("go-mongo").C("users")
+	err = c.Insert(u)
+	if err != nil {
+		fmt.Println("Could not add user:", err)
+	}
 
-	session.DB("go-mongo").C("users").FindId("5b21577a0e48dc396663b77d").One(&v)
+	v := User{}
+	err = c.Find(bson.M{"_id": u.ID}).One(&v)
+	if err != nil {
+		fmt.Println("Server Error Finding User:", err)
+	}
+	fmt.Println(v)
+
+	newUser := User{
+		Name:   "Notnew",
+		Gender: "Male",
+		Age:    55,
+		ID:     bson.NewObjectId(),
+	}
+
+	testUser := User{}
+	//compare new user to table, if already exists do not add
+	err = c.Find(bson.M{"name": "Notnew"}).One(&testUser)
+	if err == mgo.ErrNotFound {
+		err = c.Insert(newUser)
+		if err != nil {
+			fmt.Println("Could not add user:", err)
+		}
+	} else {
+		fmt.Println("User Already Exists")
+	}
 }
