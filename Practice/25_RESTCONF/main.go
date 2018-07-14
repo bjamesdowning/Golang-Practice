8a0f280ce5a3d9267d5689e3b5b9a775b99a6e35
bjamesdowning <bjamesdowning@gmail.com>
2018-07-14 10:41:08 +0300

    Post request, creating a port channel through RESTCONF

--- Practice/25_RESTCONF/main.go
+++ Practice/25_RESTCONF/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"io/ioutil"
@@ -8,18 +9,32 @@ import (
 )
 
 func main() {
-	url := "https://ios-xe-mgmt.cisco.com:9443/restconf/data/ietf-interfaces:interfaces/interface=GigabitEthernet1?fields=name;description"
+	url := "https://172.16.167.150:443/restconf/data/Cisco-IOS-XE-native:native/interface/"
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
+
+	content := []byte(`{
+		"Cisco-IOS-XE-native:Port-channel": [
+		  {
+			"name": "1", 
+			"description": "This is a port-channel interace",
+			"delay": 22222, 
+			"load-interval": 30, 
+			"mtu": 1501
+		  }
+		]
+	 }`)
+
 	client := &http.Client{
 		Transport: tr,
 	}
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(content))
 	if err != nil {
 		fmt.Println(err)
 	}
-	req.SetBasicAuth("root", "D_Vay!_10&")
+	req.SetBasicAuth("cisco", "cisco")
+	req.Header.Add("Content-Type", "application/yang-data+json")
 	req.Header.Add("Accept", "application/yang-data+json")
 	resp, err := client.Do(req)
 	if err != nil {
