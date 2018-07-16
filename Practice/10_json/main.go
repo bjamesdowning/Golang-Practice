b922a18986702cdf0b6f8b21894d9844b331da9d
bjamesdowning <bjamesdowning@gmail.com>
2018-07-16 21:40:28 +0300

    better example for handeling json

--- Practice/10_json/main.go
+++ Practice/10_json/main.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
-//create a map object to take in the json data
-//use open interface to allow various input types
-var obj map[string]interface{}
+//Msg struct used to encode/decode json
+type Msg struct {
+	Fname string `json:"fname"`
+	Lname string `json:"lname"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
 
 func main() {
+	msg := []Msg{
+		{"John", "Doe", "Jdoe@email.com", 27},
+		{"Dana", "Sumthin", "Dsumthin@email.com", 27},
+		{"Jerry", "Else", "JElse@email.com", 27},
+		{"Phil", "Other", "Pother@email.com", 27},
+	}
+
+	createJSON(msg)
+
 	f, err := os.Open("data.json")
 	if err != nil {
 		log.Println("Error: ", err)
 	}
 	defer f.Close()
 
-	json.NewDecoder(f).Decode(&obj) //Ensure Decode/Encode takes in a pointer
-	fmt.Println(obj)
+	msg2 := []Msg{}
+	dec := json.NewDecoder(f)
+	dec.Decode(&msg2)
+	fmt.Println(msg2)
+}
+
+func createJSON(msg []Msg) {
+	buf := new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	enc.Encode(msg)
+
+	f, err := os.Create("data.json")
+	if err != nil {
+		log.Println("Error: ", err)
+	}
+	defer f.Close()
+	io.Copy(f, buf)
 }
